7081cf351a7c308084365fc3b9f95bce05ec9318
tjvr <tim@tjvr.org>
2016-12-02 11:53:08 +0000

    wip game engine

--- host.js
+++ host.js
@@ -1,4 +1,6 @@
 
+const Matter = require('matter-js/build/matter.js')
+
 
 class Player {
   constructor(game, id, send) {
@@ -7,16 +9,50 @@ class Player {
     this.send = send
   }
 
+  place(json) {
+    let { emoji } = json
+    this.entity = new Entity(emoji, 0, 0)
+    this.game.add(this.entity)
+  }
+
   destroy() {
   }
 
 }
 
 
+class Entity {
+  constructor(name = '', x = 0, y = 0, rot = 0, scale = 1, opacity = 1, mass = 1) {
+    this.name = name
+    this.x = x
+    this.y = y
+    this.rot = rot
+    this.scale = scale
+    this.opacity = opacity
+    this.mass = mass
+  }
+
+  toJSON() {
+    return {
+      x: this.x,
+      y: this.y,
+      rot: this.rot,
+      scale: this.scale,
+      opacity: this.opacity,
+      mass: this.mass,
+    }
+  }
+}
+
+
+
+
 class Game {
   constructor() {
     this.players = {}
     this.entities = []
+
+    this.engine = Matter.Engine.create()
   }
 
 
@@ -43,16 +79,30 @@ class Game {
 
   handle(playerId, json) {
     let player = this.players[playerId]
-    console.log(json)
-    this.broadcast(json)
+    let type = json.type
+    delete json.type
+    this[`handle_${type}`](playerId, json)
+  }
+
+  handle_spawn(id, json) {
+    let player = this.players[playerId]
+    player.place(json.name, json.x, json.y)
   }
 
 
   /* entities */
 
-  // TODO
+  add(entity) {
+    this.entities.push(entity)
+    // TODO add to Matter.js
+  }
 
 
+  /* main loop */
+
+  tick() {
+
+  }
   
 }
 
--- server.js
+++ server.js
@@ -61,7 +61,7 @@ var server = http.createServer(function(request, response) {
 
 })
 server.listen(8080, function() {
-  console.log((new Date()) + ' Server is listening on port 8080')
+  console.log((new Date()) + ' go to http://localhost:8080/')
 })
 
 wsServer = new WebSocketServer({
