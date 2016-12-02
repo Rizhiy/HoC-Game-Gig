dc73d6d2047b3cac5239c1c27fc92cd84de75667
tjvr <tim@tjvr.org>
2016-12-02 17:03:31 +0000

    if the connection is *closed*, die

--- static/main.js
+++ static/main.js
@@ -238,6 +238,10 @@ function main(conn, emoji) {
     }
   })
 
+  conn.close(() => {
+    document.body.innerHTML = ''
+  })
+
   conn.send({ type: 'spawn', name: choose(emojiNames) });
 
     document.querySelector(".world").appendChild(addGround());
